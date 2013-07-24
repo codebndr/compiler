e4abb70e708a1c3adc1b8ed8fd11cdfba46ac4a9
Vasilis Georgitzikis <tzikis@gmail.com>
2013-07-24 00:40:17 +0000

    awesome ACL improvement for crappy cache permission issues

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -21,6 +21,27 @@ sudo php composer.phar install
 #
 #sudo setfacl -R -m u:www-data:rwX -m u:ubuntu:rwX /opt/codebender/@PACKAGENAME@/Symfony/app/cache /opt/codebender/@PACKAGENAME@/Symfony/app/logs
 #sudo setfacl -dR -m u:www-data:rwx -m u:ubuntu:rwx /opt/codebender/@PACKAGENAME@/Symfony/app/cache /opt/codebender/@PACKAGENAME@/Symfony/app/logs
+#sudo chown -R ubuntu:www-data /opt/codebender/@PACKAGENAME@/Symfony/app/cache/ /opt/codebender/@PACKAGENAME@/Symfony/app/logs
+#sudo chmod -R 775 /opt/codebender/@PACKAGENAME@/Symfony/app/cache/ /opt/codebender/@PACKAGENAME@/Symfony/app/logs
 
-sudo chown -R ubuntu:www-data /opt/codebender/@PACKAGENAME@/Symfony/app/cache/ /opt/codebender/@PACKAGENAME@/Symfony/app/logs
-sudo chmod -R 775 /opt/codebender/@PACKAGENAME@/Symfony/app/cache/ /opt/codebender/@PACKAGENAME@/Symfony/app/logs
+sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/cache/*
+sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/logs/*
+
+sudo dd if=/dev/zero of=/opt/codebender/@PACKAGENAME@/cache-fs bs=1024 count=0 seek=200000
+sudo dd if=/dev/zero of=/opt/codebender/@PACKAGENAME@/logs-fs bs=1024 count=0 seek=200000
+
+yes | sudo mkfs.ext4 /opt/codebender/@PACKAGENAME@/cache-fs
+yes | sudo mkfs.ext4 /opt/codebender/@PACKAGENAME@/logs-fs
+
+echo "/opt/codebender/@PACKAGENAME@/cache-fs /opt/codebender/@PACKAGENAME@/Symfony/app/cache ext4 loop,acl 0 0" | sudo tee -a /etc/fstab
+echo "/opt/codebender/@PACKAGENAME@/logs-fs /opt/codebender/@PACKAGENAME@/Symfony/app/logs ext4 loop,acl 0 0" | sudo tee -a /etc/fstab
+
+sudo mount /opt/codebender/@PACKAGENAME@/Symfony/app/cache/
+sudo mount /opt/codebender/@PACKAGENAME@/Symfony/app/logs/
+
+sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/cache/*
+sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/logs/*
+
+sudo setfacl -R -m u:www-data:rwX -m u:ubuntu:rwX /opt/codebender/@PACKAGENAME@/Symfony/app/cache /opt/codebender/@PACKAGENAME@/Symfony/app/logs
+sudo setfacl -dR -m u:www-data:rwx -m u:ubuntu:rwx /opt/codebender/@PACKAGENAME@/Symfony/app/cache /opt/codebender/@PACKAGENAME@/Symfony/app/logs
+
--- scripts/postrm.sh
+++ scripts/postrm.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 sudo rm /etc/apache2/sites-available/@PACKAGENAME@ /etc/apache2/sites-enabled/00-@PACKAGENAME@
+
+sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/cache/*
+sudo rm -rf /opt/codebender/@PACKAGENAME@/Symfony/app/logs/*
+
+sudo umount /opt/codebender/@PACKAGENAME@/Symfony/app/cache/
+sudo umount /opt/codebender/@PACKAGENAME@/Symfony/app/logs/
+
+sudo rm /opt/codebender/@PACKAGENAME@/cache-fs
+sudo rm /opt/codebender/@PACKAGENAME@/logs-fs
+
 sudo rm -rf /opt/codebender/@PACKAGENAME@
 sudo rmdir /opt/codebender
