64e81d9a5385a9231854564cbbc2656ec1095f61
fpapadopou <fot2803@hotmail.com>
2013-12-06 01:03:19 +0200

    Changed the way Preprocessing Handler turns Arduino code to valid c++ The whole preprocessing procedure was changed. Now it can handle any type of parameter, primitive and non-primitive, and make the respective function prototype.

--- Symfony/src/Codebender/CompilerBundle/Handler/PreprocessingHandler.php
+++ Symfony/src/Codebender/CompilerBundle/Handler/PreprocessingHandler.php
@@ -13,11 +13,7 @@ namespace Codebender\CompilerBundle\Handler;
 class PreprocessingHandler
 {
 	/**
-	\brief Generates valid C++ code from Arduino source code.
-
-	\param string $code The input source code.
-	\param string $filename (optional) The name of the input file.
-	\return Valid C++ code, the result of processing the input.
+	\brief The following functions generate valid C++ code from Arduino source code.
 
 	Arduino source code files are simplified C++ files. Thus, some preprocessing has
 	to be done to convert them to valid C++ code for the compiler to read. Some of
@@ -45,57 +41,211 @@ class PreprocessingHandler
 	- pointers to functions
 	- arrays, structs, and unions
 	 */
-	function ino_to_cpp($code, $filename = NULL)
-	{
-		// Supported primitives for parameters and return values. They are put
-		// in a string, separated by "|" to be used in regular expressions.
-		// Type "void" is put in its own variable to be more readable later on
-		// in $REGEX.
-		$VOID = "void";
-		$TYPES = array($VOID, "int", "char", "word", "short", "long", "float",
-			"byte", "boolean", "uint8_t", "uint16_t", "uint32_t", "int8_t",
-			"int16_t", "int32_t");
-		$TYPES = implode("|", $TYPES);
-		// Type qualifiers for declarators.
-		$QUALS = array("const", "volatile");
-		$QUALS = implode("|", $QUALS);
-		// Type specifiers for declarators.
-		$SPECS = array("signed", "unsigned");
-		$SPECS = implode("|", $SPECS);
-		// Matches C/C++ function definitions, has high tolerance to whitespace
-		// characters. Grouping constructs are used but no value is stored in
-		// the registers.
-		//
-		// The limitations of this regular expression are described in the
-		// comments above the function definition.
-		//
-		// Examples:
-		// int foo()
-		// int foo(void)
-		// int foo(int bar)
-		// int *foo(const int bar)
-		// int *foo(volatile int *bar, int baz)
-		$REGEX = "/^\s*((?:$SPECS)\s*)*(?:$TYPES)\s*\**\s*\w+\s*\((?:\s*(?:$VOID|((?:$QUALS)\s*)*((?:$SPECS)\s*)*(?:$TYPES)\s*\**\s*\w+\s*,?)\s*)*\)/";
 
-        // Firstly, include the Arduino header file
-		$new_code = "#include <Arduino.h>\n";
+    /**
+     \param string $code The original code of the sketch
+     \return string A copy of the code with no comments, single- or double- quoted strings
+      or pre-processor directives
+     */
+    function remove_comments_directives_quotes($code)
+    {
+        // Use a copy of the code and strip comments, pre-processor directives, single- and double-quoted strings
 
-        // Secondly, generate and add the function prototypes.
-		foreach (explode("\n", $code) as $line)
-			if (preg_match($REGEX, $line, $matches))
-				$new_code .= $matches[0].";\n";
+        $regex = "/(\'.\')|(\"(?:[^\"\\\\]|\\\\.)*\")|(\/\/.*?$)|(\/\*[^*]*(?:\*(?!\/)[^*]*)*\*\/)|(^\s*\#.*?$)/m";
 
-		// Thirdly, add a preprocessor directive for line numbering.
+        // Replace every match of the regular expression with a whitespace
+
+        $return_code = preg_replace($regex, " ", $code);
+
+        return $return_code;
+    }
+
+    /**
+     \param string $code The code returned from remove_comments_directives_quotes function
+     \return string The input code having all top level braces collapsed
+     */
+    function empty_braces($code)
+    {
+        // For every line of the code remove all the contents of top level braces
+
+        $nesting = 0;
+        $start = 0;
+        $return_code = "";
+        // Use the code as an array of characters
+        for ($i=0; $i<strlen($code); $i++){
+
+            if($code[$i] == "{"){
+                if($nesting == 0){
+                    $return_code .= substr($code, $start, $i+1 - $start);
+                }
+                $nesting++;
+                continue;
+            }
+            if($code[$i] == "}"){
+                $nesting--;
+                if($nesting == 0){
+                    $start = $i;
+                }
+                continue;
+
+            }
+        }
+        $return_code .= substr($code, $start, strlen($code)-$start);
+
+        return $return_code;
+    }
+
+    /**
+     \param string $code The code returned from empty_braces function
+     \return array An array including any prototypes found in the original code
+     */
+    function find_existing_prototypes(&$code)
+    {
+        // In this case, the original code is used. Existing prototypes are matched, stored, and then removed from the code,
+        // so that in the next step the compiler knows which prototypes should really be generated
+        $existing_prototypes = array();
+        $regex = "/[\w\[\]\*]+\s+[&\[\]\*\w\s]+\([&,\[\]\*\w\s]*\)(?=\s*;)/m";
+
+        if(preg_match_all($regex, $code, $matches)){
+            $existing_prototypes = $matches[0];
+        }
+
+        $code = preg_replace($regex, " ", $code);
+
+        return $existing_prototypes;
+    }
+
+    /**
+     \param string $code The sketch code provided to the compiler
+     \param array $existing_prototypes Array of prototypes returned by find_existing_prototypes function
+     \return string The string including the function prototypes for the code
+     */
+    function generate_prototypes($code, $existing_prototypes)
+    {
+        // This function uses a regular expression to match all function declarations, generate the
+        // respective prototype and store all the prototypes in a string
+        $regex = "/[\w\[\]\*]+\s+[&\[\]\*\w\s]+\([&,\[\]\*\w\s]*\)(?=\s*\{)/m";
+
+        $function_prototypes = "";
+        if(preg_match_all($regex, $code, $matches)){
+
+            foreach($matches[0] as $match){
+
+                if(!empty($existing_prototypes)){
+                    /*
+                     * If a prototype match has no parameters, two prototypes are generated, one with no parameters and
+                     * one with parameter void. Then the code searches if one of them already exists in the original code
+                     */
+                    if(preg_match("/\(\s*\)/", $match)){
+                        $match_void = preg_replace("/(\(\s*\))/", "(void)", $match);
+                        if(in_array($match_void, $existing_prototypes)){continue;}
+                    }
+                    // If none of the above was true, check if the prototype exists
+                    if(in_array($match, $existing_prototypes)){continue;}
+                }
+                // If everything is ok, add the prototype to the return value
+                $function_prototypes .= $match.";\n";
+            }
+        }
+        return $function_prototypes;
+    }
+
+    /**
+     \param string $code The sketch code
+     \return int The position where function prototypes should be placed
+     */
+    function insertion_position($code)
+    {
+        // Use the following regular expression to match whitespaces, single- and multiline comments and preprocessor directives
+        $regex = "/(\s+|(\/\*[^*]*(?:\*(?!\/)[^*]*)*\*\/)|(\/\/.*?$)|(\#(?:\\\\\\n|.)*))/m";
+
+        // Then find all the matches in the original code and count the offset of each one.
+        preg_match_all($regex, $code, $matches, PREG_OFFSET_CAPTURE);
+
+        $prev_position = 0;
+        $position = 0;
+        // The second offset of each matches[0] object contains the starting position (string index) of the match in the code
+        //
+        foreach($matches[0] as $match){
+            // In case of a mismatch between prev_position and the beginning index of the current match, a non matching
+            // expression exists between the last two matches. This is the position where the prototypes should be placed.
+            // In other words, this is the first line of the code that is not a whitespace, comment, or preprocessor directive
+            if($match[1] != $prev_position){
+                $position = $prev_position - 1; break;}
+            $prev_position += strlen($match[0]);
+        }
+        // If position is set to -1, there have been found no matches to the regular expression, so it must be set back to zero
+        if($position == -1){$position = 0;}
+        return $position;
+    }
+
+    /**
+     \param string $code The initial sketch code
+     \param $function_prototypes The function prototypes returned by generate_prototypes function
+     \param int $position The position to place the prototypes returned by insertion_position function
+     \return string Valid c++ code
+     */
+    function build_code($code, $function_prototypes, $position)
+    {
+
+        // To build the final code, the compiler starts adding every character of the original string, until the the position
+        // found by insertion_position is reached. Then, the function prototypes are added, as well as a preprocessor directive
+        // to fix the line numbering.
+        $line=1;
+        $return_code = "";
+        if(!($position == 0)){
+            for($i=0; $i<=$position; $i++){
+
+                $return_code .= $code[$i];
+                if($code[$i] == "\n"){
+                    $line++;
+                }
+
+            }
+        }
+
+        // Include the Arduino header file
+        $return_code .= "#include <Arduino.h>\n";
+        // Then insert the prototypes, and finally the rest of the code
+        $return_code .= $function_prototypes."#line $line\n";
+        if($position == 0)
+            $next_pos = 0;
+        else
+            $next_pos = $position +1;
+        for($i=$next_pos; $i<strlen($code); $i++){
+            $return_code .= $code[$i];
+        }
+
+        return $return_code;
+    }
+
+    function ino_to_cpp($code, $filename = NULL)
+    {
+        // Remove comments, preprocessor directives, single- and double- quotes
+        $no_comms_code = $this->remove_comments_directives_quotes($code);
+        // Remove any code between all top level braces
+        $empty_braces_code = $this->empty_braces($no_comms_code);
+        // Find already existing prototypes
+        $existing_prototypes = $this->find_existing_prototypes($empty_braces_code);
+        // Generate prototypes that do not already exist
+        $function_prototypes = $this->generate_prototypes($empty_braces_code, $existing_prototypes);
+        // Find the right place to insert the function prototypes (after any preprocessor directives, comments, before any function declaration)
+        $insertion_position = $this->insertion_position($code);
+
+        $new_code = "";
+		// Add a preprocessor directive for line numbering.
 		if ($filename)
-			$new_code .= "#line 1 \"$filename\"\n";
-		else
-			$new_code .= "#line 1\n";
-
-		// Lastly, include the input source code.
-		$new_code .= $code;
+            $new_code .= "#line 1 \"$filename\"\n";
+        else
+            $new_code .= "#line 1\n";
+        // Build the new code for the cpp file that will eventually be compiled
+        $new_code .= $this->build_code($code, $function_prototypes, $insertion_position);
 
 		return $new_code;
-	}
+
+    }
+
+
 
 	/**
 	\brief Decodes and performs validation checks on input data.
