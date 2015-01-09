0ee45573d6f838c2229ed8a73ec359343bbd2fbb
Vasileios Georgitzikis <tzikis@gmail.com>
2015-01-09 03:40:15 +0200

    Auto-Formated UtilityHandler

--- Symfony/src/Codebender/CompilerBundle/Handler/UtilityHandler.php
+++ Symfony/src/Codebender/CompilerBundle/Handler/UtilityHandler.php
@@ -13,22 +13,22 @@ namespace Codebender\CompilerBundle\Handler;
 class UtilityHandler
 {
 	/**
-	\brief Extracts the files included in a compile request.
-
-	\param string $directory The directory to extract the files to.
-	\param array $request_files The files structure, as taken from the JSON request.
-	\return A list of files or a reply message in case of error.
-
-	Takes the files structure from a compile request and creates each file in a
-	specified directory. If requested, it may create additional directories and
-	have the files placed inside them accordingly.
-
-	Also creates a new structure where each key is the file extension and the
-	associated value is an array containing the absolute paths of the file, minus
-	the extension.
-
-	In case of error, the return value is an array that has a key <b>success</b>
-	and contains the response to be sent back to the user.
+	 * \brief Extracts the files included in a compile request.
+	 *
+	 * \param string $directory The directory to extract the files to.
+	 * \param array $request_files The files structure, as taken from the JSON request.
+	 * \return A list of files or a reply message in case of error.
+	 *
+	 * Takes the files structure from a compile request and creates each file in a
+	 * specified directory. If requested, it may create additional directories and
+	 * have the files placed inside them accordingly.
+	 *
+	 * Also creates a new structure where each key is the file extension and the
+	 * associated value is an array containing the absolute paths of the file, minus
+	 * the extension.
+	 *
+	 * In case of error, the return value is an array that has a key <b>success</b>
+	 * and contains the response to be sent back to the user.
 	 */
 	function extract_files($directory, $request_files, $lib_extraction)
 	{
@@ -48,14 +48,14 @@ class UtilityHandler
 		// Examples: foo.c bar.cpp
 		$REGEX = "/(.*)\.($EXTENSIONS)$/";
 
-        if(!file_exists($directory))
-            mkdir($directory, 0777, true);
+		if (!file_exists($directory))
+			mkdir($directory, 0777, true);
 
 		foreach ($request_files as $file)
 		{
 			$filename = $file["filename"];
 			$content = $file["content"];
-            $ignore = false;
+			$ignore = false;
 
 			$failure_response = array(
 				"success" => false,
@@ -72,22 +72,22 @@ class UtilityHandler
 			{
 				$new_directory = pathinfo($filename, PATHINFO_DIRNAME);
 
-                if (($lib_extraction === true) && ($new_directory !== "utility"))
-                    $ignore = true;
-                if (!file_exists("$directory/$new_directory"))
-                    mkdir("$directory/$new_directory", 0777, true);
-                // There is no reason to check whether mkdir()
-                // succeeded, given that the call to
-                // file_put_contents() that follows would fail
-                // as well.
+				if (($lib_extraction === true) && ($new_directory !== "utility"))
+					$ignore = true;
+				if (!file_exists("$directory/$new_directory"))
+					mkdir("$directory/$new_directory", 0777, true);
+				// There is no reason to check whether mkdir()
+				// succeeded, given that the call to
+				// file_put_contents() that follows would fail
+				// as well.
 
 			}
 
 			if (file_put_contents("$directory/$filename", $content) === false)
 				return $failure_response;
 
-            if ($ignore)
-                continue;
+			if ($ignore)
+				continue;
 
 			if (preg_match($REGEX, $filename, $matches))
 				$files[$matches[2]][] = "$directory/$matches[1]";
@@ -100,11 +100,11 @@ class UtilityHandler
 	}
 
 	/**
-	\brief Searches for files with specific extensions in a directory.
-
-	\param string $directory The directory to search for files.
-	\param mixed $extensions An array of strings, the extensions to look for.
-	\return A list of files that have the appropriate extension.
+	 * \brief Searches for files with specific extensions in a directory.
+	 *
+	 * \param string $directory The directory to search for files.
+	 * \param mixed $extensions An array of strings, the extensions to look for.
+	 * \return A list of files that have the appropriate extension.
 	 */
 	function get_files_by_extension($directory, $extensions)
 	{
@@ -120,20 +120,20 @@ class UtilityHandler
 	}
 
 	/**
-	\brief Executes a command and displays the command itself and its output.
-
-	\param string $command The command to be executed.
-
-	Simplifies the creation and debugging of pages that rely on multiple external
-	programs by "emulating" the execution of the requested command in a terminal
-	emulator. Can be useful during early stages of development. Replace with
-	<b>exec()</b> afterwards.
-
-	To perform the command execution, <b>passthru()</b> is used. The string
-	<b>2\>&1</b> is appended to the command to ensure messages sent to standard
-	error are not lost.
-
-	\warning It is not possible to redirect the standard error output to a file.
+	 * \brief Executes a command and displays the command itself and its output.
+	 *
+	 * \param string $command The command to be executed.
+	 *
+	 * Simplifies the creation and debugging of pages that rely on multiple external
+	 * programs by "emulating" the execution of the requested command in a terminal
+	 * emulator. Can be useful during early stages of development. Replace with
+	 * <b>exec()</b> afterwards.
+	 *
+	 * To perform the command execution, <b>passthru()</b> is used. The string
+	 * <b>2\>&1</b> is appended to the command to ensure messages sent to standard
+	 * error are not lost.
+	 *
+	 * \warning It is not possible to redirect the standard error output to a file.
 	 */
 	function debug_exec($command, /** @noinspection PhpUnusedParameterInspection */
 	                    &$output, /** @noinspection PhpUnusedParameterInspection */
