7c608ab4c0932a2b2a1d8f7a1cde8285b9374b76
Ethan Green <erg53@cornell.edu>
2015-02-24 14:40:03 -0500

    Added extractFiles() to handler

--- Symfony/src/Codebender/CompilerBundle/Handler/ArduinoCommandHandler.php
+++ Symfony/src/Codebender/CompilerBundle/Handler/ArduinoCommandHandler.php
@@ -11,29 +11,37 @@
 
 namespace Codebender\CompilerBundle\Handler;
 
+
+require_once("System.php");
 use System;
 
 class ArduinoCommandHandler
 {
 
-    function main($request, $params)
+    function main($request)
     {
+	//Todo: Test Input Validiy
 
-	$reply = $request;
+	//Todo: Extract files from request
+	$eFile = $this->extractFiles($request);
 
-	//$eFile = extractFiles($request);
+	//Return output in reply
+	$reply = $eFile;
 
-	//$reply = fopen($eFile);
-
-        return array("status" => "success", "returnfile" => $reply["test"]);
+        return array("status" => "success", "returnfile" => $reply);
 
     }
 
-    private function extractFiles($request, $params)
+    private function extractFiles()
     {
 	// Extract the file from the array and save to /tmp
 	// Return the file path
+	
+	//$temp_dir = sys_get_temp_dir();
+	//$filename = "temp1";
+	//$tempfile = tempnam($temp_dir, $filename);
 
-
+	$tempfile = "Filename";
+	return $tempfile;
     }
 }
