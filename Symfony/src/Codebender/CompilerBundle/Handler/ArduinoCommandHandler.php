983fc1372445a7a280448730ac7e6a401abc410c
Ethan Green <erg53@cornell.edu>
2015-02-26 22:21:01 -0500

    Basic tmp file creation with tempnam()

--- Symfony/src/Codebender/CompilerBundle/Handler/ArduinoCommandHandler.php
+++ Symfony/src/Codebender/CompilerBundle/Handler/ArduinoCommandHandler.php
@@ -23,25 +23,34 @@ class ArduinoCommandHandler
 	//Todo: Test Input Validiy
 
 	//Todo: Extract files from request
-	$eFile = $this->extractFiles($request);
+	$tempfiles = $this->extractFiles($request);
 
 	//Return output in reply
-	$reply = $eFile;
-
-        return array("status" => "success", "returnfile" => $reply);
+	$reply = $tempfiles['files'];
+	$handle = fopen($tempfiles['files'],"r+");
+	fclose($handle);
+	unlink($tempfiles['files']);
+        return array("success" => "false","step" =>"0", "message" => $reply);
 
     }
 
-    private function extractFiles()
+    private function extractFiles($request)
     {
 	// Extract the file from the array and save to /tmp
 	// Return the file path
-	
-	//$temp_dir = sys_get_temp_dir();
-	//$filename = "temp1";
-	//$tempfile = tempnam($temp_dir, $filename);
 
-	$tempfile = "Filename";
-	return $tempfile;
+	$filename = $request['files'];
+	
+
+	$tempfile = tempnam(sys_get_temp_dir(),$filename);
+	$tempfiles = array("files" => $tempfile);
+	return $tempfiles;
     }
 }
+
+
+
+
+
+
+
