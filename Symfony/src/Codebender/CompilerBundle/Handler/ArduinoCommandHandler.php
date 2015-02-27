31c8f7aea20df8767b240ac033e26ee72dbd4daa
Ethan Green <erg53@cornell.edu>
2015-02-27 10:29:13 -0500

    Added file structure parsing and deletion

--- Symfony/src/Codebender/CompilerBundle/Handler/ArduinoCommandHandler.php
+++ Symfony/src/Codebender/CompilerBundle/Handler/ArduinoCommandHandler.php
@@ -23,11 +23,13 @@ class ArduinoCommandHandler
 	//Todo: Test Input Validiy
 
 	//Todo: Extract files from request
-	$tempfiles = $this->extractFiles($request);
 
-	
-	$reply = $request;	
-        return array("success" => "false","step" =>"0", "message" => $reply);
+	//Todo: Escape output and harden security
+
+	$tempfiles = $this->extractFiles($request);	
+
+	$this->deleteFiles($tempfiles);
+        return array("success" => "true","step" =>"0", "message" => $tempfiles);
 
     }
 
@@ -38,20 +40,33 @@ class ArduinoCommandHandler
 
 	$tempfiles = array();
 
-	foreach ($request['files'] as $key => $val) {
+	foreach ($request['files'] as $file => $contents) {
+	    foreach ($contents as $key => $val) {
 
-	if ($key == 'filename') {
-		$filename = tempnam(sys_get_temp_dir(),$val);
-		array_push($tempfiles,$filename);
-	} elseif ($key == 'content') {
+    	    if ($key == 'filename') {
+		    $extension = pathinfo($key,PATHINFO_EXTENSION);
+		    $filename = tempnam(sys_get_temp_dir(),$val);
+		    $extfilename = $filename . '.' . $extension;
+		    if(!rename($filename,$extfilename)) {
+			return false;
+			}
+	            array_push($tempfiles,$newfilename);
+		} elseif ($key == 'content') {
 
-		$tempfile = end($files);
-		$handle = fopen($tempfile,"r+");
-		fwrite($handle,$val);
-		fclose($handle);
-	}
+		    $tempfile = end($files);
+		    $handle = fopen($tempfile,"r+");
+		    fwrite($handle,$val);
+		    fclose($handle);
+		}
 	}
 	return $tempfiles;
+    }
+
+    private function deleteFiles($fileList) {
+	foreach ($filelist as $file) {
+	    unlink($file);	
+	}
+
     }
 }
 
