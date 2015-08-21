ab69a71eb1824c5ed04cdfbc8e4b2849ec4157f3
Fotis Papadopoulos <fot2803@hotmail.com>
2015-08-21 15:18:18 +0300

    Code formatting in tests class

--- Symfony/src/Codebender/CompilerBundle/Tests/Controller/DefaultControllerFunctionalTest.php
+++ Symfony/src/Codebender/CompilerBundle/Tests/Controller/DefaultControllerFunctionalTest.php
@@ -6,148 +6,157 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerFunctionalTest extends WebTestCase
 {
-	public function testStatus() {
-		$client = static::createClient();
+    public function testStatus()
+    {
+        $client = static::createClient();
 
-		$client->request('GET', '/status');
+        $client->request('GET', '/status');
 
-		$this->assertEquals($client->getResponse()->getContent(), '{"success":true,"status":"OK"}');
+        $this->assertEquals($client->getResponse()->getContent(), '{"success":true,"status":"OK"}');
 
-	}
+    }
 
-	public function testInvalidKey() {
-		$client = static::createClient();
+    public function testInvalidKey()
+    {
+        $client = static::createClient();
 
-		$client->request('GET', '/inValidKey/v1');
+        $client->request('GET', '/inValidKey/v1');
 
-		$this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid authorization key."}');
+        $this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid authorization key."}');
 
-	}
+    }
 
-	public function testInvalidAPI() {
-		$client = static::createClient();
+    public function testInvalidAPI()
+    {
+        $client = static::createClient();
 
-		$authorizationKey = $client->getContainer()->getParameter("authorizationKey");
+        $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-		$client->request('GET', '/'.$authorizationKey.'/v666');
+        $client->request('GET', '/' . $authorizationKey . '/v666');
 
-		$this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid API version."}');
+        $this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid API version."}');
 
-	}
+    }
 
-	public function testInvalidInput() {
-		$client = static::createClient();
+    public function testInvalidInput()
+    {
+        $client = static::createClient();
 
-		$authorizationKey = $client->getContainer()->getParameter("authorizationKey");
+        $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-		$client->request('GET', '/'.$authorizationKey.'/v1');
+        $client->request('GET', '/' . $authorizationKey . '/v1');
 
-		$this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid input."}');
+        $this->assertEquals($client->getResponse()->getContent(), '{"success":false,"step":0,"message":"Invalid input."}');
 
-	}
+    }
 
-	public function testBlinkUnoSyntaxCheck() {
-		$files = array(array("filename" => "Blink.ino", "content" => "int led = 13;\nvoid setup() {pinMode(led, OUTPUT);}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\ndelay(1000);\n}\n"));
-		$format = "syntax";
-		$version = "105";
-		$libraries = array();
-		$build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
+    public function testBlinkUnoSyntaxCheck()
+    {
+        $files = array(array("filename" => "Blink.ino", "content" => "int led = 13;\nvoid setup() {pinMode(led, OUTPUT);}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\ndelay(1000);\n}\n"));
+        $format = "syntax";
+        $version = "105";
+        $libraries = array();
+        $build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
 
-		$data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
+        $data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
 
-		$client = static::createClient();
+        $client = static::createClient();
 
-		$authorizationKey = $client->getContainer()->getParameter("authorizationKey");
+        $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-		$client->request('POST', '/'.$authorizationKey.'/v1', array(),array(),array(),$data);
+        $client->request('POST', '/' . $authorizationKey . '/v1', array(), array(), array(), $data);
 
-		$response = json_decode($client->getResponse()->getContent(), true);
+        $response = json_decode($client->getResponse()->getContent(), true);
 
-		$this->assertEquals($response["success"], true);
-		$this->assertTrue(is_numeric($response["time"]));
+        $this->assertEquals($response["success"], true);
+        $this->assertTrue(is_numeric($response["time"]));
 
-	}
+    }
 
-	public function testBlinkUnoCompile() {
-		$files = array(array("filename" => "Blink.ino", "content" => "\nint led = 13;\nvoid setup() {\npinMode(led, OUTPUT);\n}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\ndelay(1000);\n}\n"));
-		$format = "binary";
-		$version = "105";
-		$libraries = array();
-		$build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
+    public function testBlinkUnoCompile()
+    {
+        $files = array(array("filename" => "Blink.ino", "content" => "\nint led = 13;\nvoid setup() {\npinMode(led, OUTPUT);\n}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\ndelay(1000);\n}\n"));
+        $format = "binary";
+        $version = "105";
+        $libraries = array();
+        $build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
 
-		$data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
+        $data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
 
-		$client = static::createClient();
+        $client = static::createClient();
 
-		$authorizationKey = $client->getContainer()->getParameter("authorizationKey");
+        $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-		$client->request('POST', '/'.$authorizationKey.'/v1', array(), array(), array(), $data);
+        $client->request('POST', '/' . $authorizationKey . '/v1', array(), array(), array(), $data);
 
-		$response = json_decode($client->getResponse()->getContent(), true);
+        $response = json_decode($client->getResponse()->getContent(), true);
 
-		$this->assertEquals($response["success"], true);
-		$this->assertTrue(is_numeric($response["time"]));
-		$this->assertTrue(is_numeric($response["size"]));
+        $this->assertEquals($response["success"], true);
+        $this->assertTrue(is_numeric($response["time"]));
+        $this->assertTrue(is_numeric($response["size"]));
 
         $objectFilesPath = $client->getContainer()->getParameter('temp_dir') . '/' . $client->getContainer()->getParameter('objdir');
         $coreObjectLibrary = glob("$objectFilesPath/*__v105__hardware__arduino__cores__arduino________atmega328p_16000000_arduino_standard_null_null_______core.a");
         $this->assertTrue(count($coreObjectLibrary) > 0);
-	}
+    }
 
-	public function testBlinkUnoSyntaxCheckError() {
-		$files = array(array("filename" => "Blink.ino", "content" => "\nint led = 13\nvoid setup() {\npinMode(led, OUTPUT);\npinMode(led);\n}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\ndelay(1000);\n}\n"));
-		$format = "syntax";
-		$version = "105";
-		$libraries = array();
-		$build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
+    public function testBlinkUnoSyntaxCheckError()
+    {
+        $files = array(array("filename" => "Blink.ino", "content" => "\nint led = 13\nvoid setup() {\npinMode(led, OUTPUT);\npinMode(led);\n}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\ndelay(1000);\n}\n"));
+        $format = "syntax";
+        $version = "105";
+        $libraries = array();
+        $build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
 
-		$data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
+        $data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
 
-		$client = static::createClient();
+        $client = static::createClient();
 
-		$authorizationKey = $client->getContainer()->getParameter("authorizationKey");
+        $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-		$client->request('POST', '/'.$authorizationKey.'/v1', array(), array(), array(), $data);
+        $client->request('POST', '/' . $authorizationKey . '/v1', array(), array(), array(), $data);
 
-		$response = json_decode($client->getResponse()->getContent(), true);
+        $response = json_decode($client->getResponse()->getContent(), true);
 
-		$this->assertEquals($response["success"], false);
-		$this->assertEquals($response["success"], false);
-		$this->assertEquals($response["step"], 4);
-		$this->assertContains("Blink.ino:2:13:", $response["message"]);
-		$this->assertContains("expected ';' after top level declarator", $response["message"]);
-		$this->assertContains("no matching function for call to 'pinMode'", $response["message"]);
-		$this->assertContains("candidate function not viable: requires 2 arguments, but 1 was provided", $response["message"]);
-		// $this->assertContains("2 errors generated.", $response["message"]); //unfortunately we no longer show how many errors were generated
-	}
+        $this->assertEquals($response["success"], false);
+        $this->assertEquals($response["success"], false);
+        $this->assertEquals($response["step"], 4);
+        $this->assertContains("Blink.ino:2:13:", $response["message"]);
+        $this->assertContains("expected ';' after top level declarator", $response["message"]);
+        $this->assertContains("no matching function for call to 'pinMode'", $response["message"]);
+        $this->assertContains("candidate function not viable: requires 2 arguments, but 1 was provided", $response["message"]);
+        // $this->assertContains("2 errors generated.", $response["message"]); //unfortunately we no longer show how many errors were generated
+    }
 
-	public function testBlinkUnoCompileError() {
-		$files = array(array("filename" => "Blink.ino", "content" => "\nint led = 13\nvoid setup() {\npinMode(led, OUTPUT);\npinMode(led);\n}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\n  delay(1000);\n}\n"));
-		$format = "binary";
-		$version = "105";
-		$libraries = array();
-		$build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
+    public function testBlinkUnoCompileError()
+    {
+        $files = array(array("filename" => "Blink.ino", "content" => "\nint led = 13\nvoid setup() {\npinMode(led, OUTPUT);\npinMode(led);\n}\nvoid loop() {\ndigitalWrite(led, HIGH);\ndelay(1000);\ndigitalWrite(led, LOW);\n  delay(1000);\n}\n"));
+        $format = "binary";
+        $version = "105";
+        $libraries = array();
+        $build = array("mcu" => "atmega328p", "f_cpu" => "16000000", "core" => "arduino", "variant" => "standard");
 
-		$data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
+        $data = json_encode(array("files" => $files, "format" => $format, "version" => $version, "libraries" => $libraries, "build" => $build));
 
-		$client = static::createClient();
+        $client = static::createClient();
 
-		$authorizationKey = $client->getContainer()->getParameter("authorizationKey");
+        $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-		$client->request('POST', '/'.$authorizationKey.'/v1', array(), array(), array(), $data);
+        $client->request('POST', '/' . $authorizationKey . '/v1', array(), array(), array(), $data);
 
-		$response = json_decode($client->getResponse()->getContent(), true);
+        $response = json_decode($client->getResponse()->getContent(), true);
 
-		$this->assertEquals($response["success"], false);
-		$this->assertEquals($response["step"], 4);
-		$this->assertContains("Blink.ino:2:13:", $response["message"]);
-		$this->assertContains("expected ';' after top level declarator", $response["message"]);
-		$this->assertContains("no matching function for call to 'pinMode'", $response["message"]);
-		$this->assertContains("candidate function not viable: requires 2 arguments, but 1 was provided", $response["message"]);
-		// $this->assertContains("2 errors generated.", $response["message"]);  //unfortunately we no longer show how many errors were generated
-	}
+        $this->assertEquals($response["success"], false);
+        $this->assertEquals($response["step"], 4);
+        $this->assertContains("Blink.ino:2:13:", $response["message"]);
+        $this->assertContains("expected ';' after top level declarator", $response["message"]);
+        $this->assertContains("no matching function for call to 'pinMode'", $response["message"]);
+        $this->assertContains("candidate function not viable: requires 2 arguments, but 1 was provided", $response["message"]);
+        // $this->assertContains("2 errors generated.", $response["message"]);  //unfortunately we no longer show how many errors were generated
+    }
 
-    public function testExternalVariant() {
+    public function testExternalVariant()
+    {
         $files = array(array('filename' => 'Blink.ino', 'content' => "void setup(){}\nvoid loop(){}\n"));
         $format = 'binary';
         $version = '105';
@@ -159,7 +168,7 @@ class DefaultControllerFunctionalTest extends WebTestCase
 
         $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-        $client->request('POST', '/'.$authorizationKey.'/v1', array(), array(), array(), $data);
+        $client->request('POST', '/' . $authorizationKey . '/v1', array(), array(), array(), $data);
 
         $response = json_decode($client->getResponse()->getContent(), true);
 
@@ -170,7 +179,8 @@ class DefaultControllerFunctionalTest extends WebTestCase
         $this->assertTrue(count($coreObjectLibrary) > 0);
     }
 
-    public function testExternalCore() {
+    public function testExternalCore()
+    {
         $files = array(array('filename' => 'Blink.ino', 'content' => "void setup(){}\nvoid loop(){}\n"));
         $format = 'binary';
         $version = '105';
@@ -182,7 +192,7 @@ class DefaultControllerFunctionalTest extends WebTestCase
 
         $authorizationKey = $client->getContainer()->getParameter("authorizationKey");
 
-        $client->request('POST', '/'.$authorizationKey.'/v1', array(), array(), array(), $data);
+        $client->request('POST', '/' . $authorizationKey . '/v1', array(), array(), array(), $data);
 
         $response = json_decode($client->getResponse()->getContent(), true);
 
@@ -194,19 +204,20 @@ class DefaultControllerFunctionalTest extends WebTestCase
         $this->assertTrue(count($coreObjectLibrary) > 0);
     }
 
-    public function testArchiveIsCreated() {
+    public function testArchiveIsCreated()
+    {
         $files = array(array('filename' => 'Blink.ino', 'content' => "void setup(){}\nvoid loop(){}\n"));
         $format = 'binary';
         $version = '105';
         $libraries = array();
         $build = array('mcu' => 'atmega328p', 'f_cpu' => '16000000', 'core' => 'arduino', 'variant' => 'standard');
-        $data = json_encode(array('files' => $files, 'archive' => true,  'format' => $format, 'version' => $version, 'libraries' => $libraries, 'build' => $build));
+        $data = json_encode(array('files' => $files, 'archive' => true, 'format' => $format, 'version' => $version, 'libraries' => $libraries, 'build' => $build));
 
         $client = static::createClient();
 
         $authorizationKey = $client->getContainer()->getParameter('authorizationKey');
 
-        $client->request('POST', '/'.$authorizationKey.'/v1', array(), array(), array(), $data);
+        $client->request('POST', '/' . $authorizationKey . '/v1', array(), array(), array(), $data);
 
         $response = json_decode($client->getResponse()->getContent(), true);
 
@@ -214,11 +225,13 @@ class DefaultControllerFunctionalTest extends WebTestCase
         $this->assertTrue(file_exists($response['archive']));
     }
 
-    public function testAutocomplete() {
+    public function testAutocomplete()
+    {
         $this->markTestIncomplete('No tests for the code completion feature yet.');
     }
 
-	public function testIncorrectInputs() {
-		$this->markTestIncomplete('No tests for invalid inputs yet');
-	}
+    public function testIncorrectInputs()
+    {
+        $this->markTestIncomplete('No tests for invalid inputs yet');
+    }
 }
